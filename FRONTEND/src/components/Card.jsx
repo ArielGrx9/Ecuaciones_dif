758c2a675cf2da9fdb92d346a25255fd16b6fd11
Linn616 <link333super@gmail.com>
2025-11-18 18:50:49 -0600

    Pueba 2

--- FRONTEND/src/components/Card.jsx
+++ FRONTEND/src/components/Card.jsx
@@ -1,4 +1,79 @@
+import { useState } from "react";
+
+function validarExpreciones(expr) {
+  let normalizar = expr.toLowerCase();
+  // ach dime si quieres mas mmds despues de las 7:30 no puedo
+
+  //Derivada
+  normalizar = normalizar.replace(/\bdev\b/g, "der");
+
+  // Seno
+  normalizar = normalizar.replace(/\bsen\b/g, "sin");
+  normalizar = normalizar.replace(/\bseno\b/g, "sin");
+
+  // Coseno
+  normalizar = normalizar.replace(/\bcoseno\b/g, "cos");
+
+  // Tangente
+  normalizar = normalizar.replace(/\btan\b/g, "tan");
+  normalizar = normalizar.replace(/\btangente\b/g, "tan");
+
+  // Raíz
+  normalizar = normalizar.replace(/\braiz\b/g, "sqrt");
+  normalizar = normalizar.replace(/\braíz\b/g, "sqrt");
+  // Notación como √x
+  normalizar = normalizar.replace(/√\(/g, "sqrt(");
+  normalizar = normalizar.replace(/√([a-z0-9]+)/gi, "sqrt($1)");
+
+  // Logaritmos
+  normalizar = normalizar.replace(/\blogaritmo\b/g, "log");
+  normalizar = normalizar.replace(/\blog10\b/g, "log10");
+  normalizar = normalizar.replace(/\bln\b/g, "ln");
+  normalizar = normalizar.replace(/\blog natural\b/g, "ln");
+
+  // Variaciones de "e"
+  normalizar = normalizar.replace(/\be\^/g, "exp("); // e^x → exp(x
+  normalizar = normalizar.replace(/\bexp\(/g, "exp("); // exp → exp
+
+  // e^x sin paréntesis: e^x → exp(x)
+  normalizar = normalizar.replace(/e\^([a-z0-9]+)/g, "exp($1)");
+
+  // potencias
+  normalizar = normalizar.replace(/\bcuadrado\b/g, "^2");
+  normalizar = normalizar.replace(/\bcubico\b/g, "^3");
+  normalizar = normalizar.replace(/\bcúbico\b/g, "^3");
+
+  normalizar = normalizar.replace(/\bpi\b/g, "pi"); // ya normal
+  normalizar = normalizar.replace(/\bπ\b/g, "pi");
+
+  // y''''  -> diff(y, x, 4)
+  normalizar = normalizar.replace(/([a-z])''''\b/g, "diff($1, x, 4)");
+
+  // y'''  -> diff(y, x, 3)
+  normalizar = normalizar.replace(/([a-z])'''\b/g, "diff($1, x, 3)");
+
+  // y''  -> diff(y, x, 2)
+  normalizar = normalizar.replace(/([a-z])''\b/g, "diff($1, x, 2)");
+
+  // y'  -> diff(y, x)
+  normalizar = normalizar.replace(/([a-z])'\b/g, "diff($1, x)");
+
+  return normalizar;
+}
+
 function EcDif() {
+  const [input, setInput] = useState("");
+  const [type, setType] = useState("Homogénea");
+  const [resultExpression, setResultExpression] = useState("");
+
+  const handleSend = () => {
+    const normalizar = validarExpreciones(input);
+    console.log(normalizar);
+    console.log(input);
+    console.log(type);
+    setResultExpression(normalizar);
+  };
+
   return (
     <div className="container py-5">
       {/* Título */}
@@ -16,8 +91,25 @@ function EcDif() {
               type="text"
               className="form-control"
               placeholder="f(x) = ..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <button className="btn btn-danger">Ir</button>
+
+            {/* Boton */}
+            <button className="btn btn-danger" onChange={handleSend}>
+              Ir
+            </button>
+
+            {/* Menu */}
+            <select
+              className="form-select"
+              style={{ maxWidth: "180px" }}
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="Homogénea">Homogénea</option>
+              <option value="No homogénea">No homogénea</option>
+            </select>
           </div>
 
           <small className="text-muted">Ejemplo: x^2 + 3x - 2</small>
@@ -64,6 +156,15 @@ function EcDif() {
           </div>
         </div>
       </div>
+      <footer className="bg-dark text-white py-3 mt-5">
+        <div className="container text-center">
+          <p className="mb-1">Segundo Parcial Ecuaciones Diferenciales</p>
+          <small>
+            Desarrollado por Johan Uriel Marin Viñas Angel Ariel Garcia Ulices
+            Karsten Cruz Dereck
+          </small>
+        </div>
+      </footer>
     </div>
   );
 }
